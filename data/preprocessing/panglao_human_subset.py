fd52aecfe029e1aa4c8d441c27f6c1e2733110f1
Amrithya.B <amrithyarao1999@gmail.com>
2025-05-14 16:38:00 +0200

    Update panglao_human_subset.py

--- data/preprocessing/panglao_human_subset.py
+++ data/preprocessing/panglao_human_subset.py
@@ -3,7 +3,14 @@ import scanpy as sc
 file_path = '/data1/data/corpus/panglao_human.h5ad'
 adata = sc.read_h5ad(file_path)
 
-print(adata)
-print(adata.X.shape)
+gene_expression_counts = (adata.X > 0).sum(axis=0)
 
-print(adata.X)
+if hasattr(gene_expression_counts, 'A1'):
+    gene_expression_counts = gene_expression_counts.A1
+
+gene_names = adata.var_names
+gene_counts = dict(zip(gene_names, gene_expression_counts))
+
+top_genes = sorted(gene_counts.items(), key=lambda x: x[1], reverse=True)[:10]
+for gene, count in top_genes:
+    print(f"{gene}: {count} cells")
