03508b7bfbd10c9cb53d871a6e1c5b8c07f21896
BALAJI Amrithya <amrithya.balaji@sms-cluster.lis-lab.fr>
2025-05-15 13:39:27 +0200

    updating subset

--- data/preprocessing/panglao_human_subset.py
+++ data/preprocessing/panglao_human_subset.py
@@ -1,19 +1,8 @@
 import scanpy as sc
 import numpy as np
 
-adata = sc.read_h5ad("/data1/data/corpus/panglao_human.h5ad")
-gene_counts = (adata.X > 0).sum(axis=0)
+adata = sc.read_h5ad("/data1/data/corpus/panglao_human10k.h5ad")
+adata_5k = sc.read_h5ad("/data1/data/corpus/panglao_human5k.h5ad")
 
-if hasattr(gene_counts, "A1"):
-    gene_counts = gene_counts.A1
-
-gene_names = adata.var_names
-sorted_indices = np.argsort(-gene_counts)
-top10k_genes = gene_names[sorted_indices[:10000]]
-top5k_genes = gene_names[sorted_indices[:5000]]
-
-adata_10k = adata[:, top10k_genes].copy()
-adata_5k = adata[:, top5k_genes].copy()
-
-adata_10k.write("/data1/data/corpus/panglao_human10k.h5ad")
-adata_5k.write("/data1/data/corpus/panglao_human5k.h5ad")
+print("10k genes dataset shape:", adata.shape)  # (n_cells, 10000)
+print("5k genes dataset shape:", adata_5k.shape)
