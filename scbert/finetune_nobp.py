2d39f84c777891d966d7def7338b5fcf88e6caa1
Amrithya.B <amrithyarao1999@gmail.com>
2025-05-21 09:51:20 +0200

    Update finetune_nobp.py

--- scbert/finetune_nobp.py
+++ scbert/finetune_nobp.py
@@ -1,29 +1,22 @@
 # -*- coding: utf-8 -*-
 import os
-import gc
 import argparse
-import json
-import random
-import math
-from functools import reduce
 import numpy as np
 import pandas as pd
-from scipy import sparse
-from sklearn.model_selection import StratifiedShuffleSplit
-from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, classification_report
+import pickle as pkl
 import torch
-from torch import nn
-from torch.utils.data import DataLoader, Dataset
+import random
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
 from torch.utils.data.distributed import DistributedSampler
 from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.distributed as dist
-
 from performer_pytorch import PerformerLM
 import scanpy as sc
-import anndata as ad
-from utils import *
-import pickle as pkl
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.metrics import classification_report, confusion_matrix
 
+# Initialize distributed training
 parser = argparse.ArgumentParser()
 parser.add_argument("--local_rank","--local-rank", type=int, default=-1)
 parser.add_argument("--bin_num", type=int, default=5)
@@ -31,31 +24,22 @@ parser.add_argument("--gene_num", type=int, default=16906)
 parser.add_argument("--epoch", type=int, default=100)
 parser.add_argument("--seed", type=int, default=2021)
 parser.add_argument("--batch_size", type=int, default=3)
-parser.add_argument("--learning_rate", type=float, default=1e-4)
-parser.add_argument("--grad_acc", type=int, default=60)
-parser.add_argument("--valid_every", type=int, default=1)
 parser.add_argument("--pos_embed", type=bool, default=True)
 parser.add_argument("--data_path", type=str, default='./data/Zheng68K.h5ad')
 parser.add_argument("--model_path", type=str, default='./panglao_pretrained.pth')
 parser.add_argument("--ckpt_dir", type=str, default='./ckpts/')
-parser.add_argument("--model_name", type=str, default='finetune')
-args = parser.parse_args()
+parser.add_argument("--model_name", type=str, default='finetune_ready')
 
-rank = int(os.environ["RANK"])
+args = parser.parse_args()
 local_rank = args.local_rank
 is_master = local_rank == 0
 
-SEED = args.seed
-BATCH_SIZE = args.batch_size
-SEQ_LEN = args.gene_num + 1
-POS_EMBED_USING = args.pos_embed
-CLASS = args.bin_num + 2
-
-seed_all(SEED + torch.distributed.get_rank())
 dist.init_process_group(backend='nccl')
 torch.cuda.set_device(local_rank)
 device = torch.device("cuda", local_rank)
-world_size = torch.distributed.get_world_size()
+
+CLASS = args.bin_num + 2
+SEQ_LEN = args.gene_num + 1
 
 class SCDataset(Dataset):
     def __init__(self, data, label):
@@ -68,32 +52,52 @@ class SCDataset(Dataset):
         full_seq[full_seq > (CLASS - 2)] = CLASS - 2
         full_seq = torch.from_numpy(full_seq).long()
         full_seq = torch.cat((full_seq, torch.tensor([0]))).to(device)
-        seq_label = self.label[rand_start]
-        return full_seq, seq_label
+        return full_seq, self.label[rand_start]
 
     def __len__(self):
         return self.data.shape[0]
 
+class Identity(nn.Module):
+    def __init__(self, dropout=0., h_dim=100, out_dim=10):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 1, (1, 200))
+        self.act = nn.ReLU()
+        self.fc1 = nn.Linear(SEQ_LEN, 512)
+        self.act1 = nn.ReLU()
+        self.dropout1 = nn.Dropout(dropout)
+        self.fc2 = nn.Linear(512, h_dim)
+        self.act2 = nn.ReLU()
+        self.dropout2 = nn.Dropout(dropout)
+        self.fc3 = nn.Linear(h_dim, out_dim)
+
+    def forward(self, x):
+        x = x[:,None,:,:]
+        x = self.conv1(x)
+        x = self.act(x)
+        x = x.view(x.shape[0],-1)
+        x = self.fc1(x)
+        x = self.act1(x)
+        x = self.dropout1(x)
+        x = self.fc2(x)
+        x = self.act2(x)
+        x = self.dropout2(x)
+        return self.fc3(x)
+
 data = sc.read_h5ad(args.data_path)
-label_dict, label = np.unique(np.array(data.obs['celltype']), return_inverse=True)
-with open('label_dict', 'wb') as fp:
-    pkl.dump(label_dict, fp)
-with open('label', 'wb') as fp:
-    pkl.dump(label, fp)
+label_dict, label = np.unique(data.obs['celltype'], return_inverse=True)
 label = torch.from_numpy(label)
 data = data.X
 
-sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=SEED)
-for index_train, index_val in sss.split(data, label):
-    data_train, label_train = data[index_train], label[index_train]
-    data_val, label_val = data[index_val], label[index_val]
-    train_dataset = SCDataset(data_train, label_train)
-    val_dataset = SCDataset(data_val, label_val)
+if is_master:
+    with open('label_dict.pkl', 'wb') as f:
+        pkl.dump(label_dict, f)
+    with open('label.pkl', 'wb') as f:
+        pkl.dump(label, f)
 
-train_sampler = DistributedSampler(train_dataset)
-val_sampler = DistributedSampler(val_dataset)
-train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, sampler=train_sampler)
-val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, sampler=val_sampler)
+sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=args.seed)
+for train_idx, val_idx in sss.split(data, label):
+    train_dataset = SCDataset(data[train_idx], label[train_idx])
+    val_dataset = SCDataset(data[val_idx], label[val_idx])
 
 model = PerformerLM(
     num_tokens=CLASS,
@@ -101,75 +105,44 @@ model = PerformerLM(
     depth=6,
     max_seq_len=SEQ_LEN,
     heads=10,
-    local_attn_heads=0,
-    g2v_position_emb=POS_EMBED_USING
+    g2v_position_emb=args.pos_embed
 )
 
 ckpt = torch.load(args.model_path)
 model.load_state_dict(ckpt['model_state_dict'])
-model.to_out = nn.Identity()
+
+for param in model.parameters():
+    param.requires_grad = False
+for param in model.norm.parameters():
+    param.requires_grad = True
+for param in model.performer.net.layers[-2].parameters():
+    param.requires_grad = True
+
+model.to_out = Identity(dropout=0., h_dim=128, out_dim=len(label_dict))
 model = model.to(device)
-model = DDP(model, device_ids=[local_rank], output_device=local_rank)
-
-loss_fn = nn.CrossEntropyLoss().to(local_rank)
-UNASSIGN_THRES = 0.0
+model = DDP(model, device_ids=[local_rank])
 
 model.eval()
-dist.barrier()
+val_loader = DataLoader(val_dataset, batch_size=args.batch_size, 
+                       sampler=DistributedSampler(val_dataset))
+
 with torch.no_grad():
-    running_loss = 0.0
-    cum_acc = 0.0
-    for index, (data, labels) in enumerate(train_loader):
-        index += 1
-        data, labels = data.to(device), labels.to(device)
-        logits = model(data)
-        loss = loss_fn(logits, labels)
-        running_loss += loss.item()
-
-        softmax = nn.Softmax(dim=-1)
-        final = softmax(logits).argmax(dim=-1)
-        pred_num = labels.size(0)
-        correct_num = torch.eq(final, labels).sum(dim=-1)
-        cum_acc += torch.true_divide(correct_num, pred_num).mean().item()
-
-    epoch_loss = running_loss / index
-    epoch_acc = 100 * cum_acc / index
-    epoch_loss = get_reduced(epoch_loss, local_rank, 0, world_size)
-    epoch_acc = get_reduced(epoch_acc, local_rank, 0, world_size)
+    data_val, labels_val = next(iter(val_loader))
+    logits = model(data_val) 
+    preds = logits.argmax(dim=-1)
+    
     if is_master:
-        print(f'== Inference | Loss: {epoch_loss:.6f} | Accuracy: {epoch_acc:6.4f}% ==')
+        print("\nConfusion matrix:")
+        print(confusion_matrix(labels_val.cpu(), preds.cpu()))
 
-predictions = []
-truths = []
-running_loss = 0.0
-model.eval()
+if is_master:
+    os.makedirs(args.ckpt_dir, exist_ok=True)
+    save_path = os.path.join(args.ckpt_dir, f"{args.model_name}.pth")
+    torch.save({
+        'model_state_dict': model.module.state_dict(),
+        'label_dict': label_dict,
+        'args': vars(args)
+    }, save_path)
+    print(f"\nModel successfully saved to {save_path}")
+
 dist.barrier()
-with torch.no_grad():
-    for index, (data_v, labels_v) in enumerate(val_loader):
-        index += 1
-        data_v, labels_v = data_v.to(device), labels_v.to(device)
-        logits = model(data_v)
-        loss = loss_fn(logits, labels_v)
-        running_loss += loss.item()
-
-        softmax = nn.Softmax(dim=-1)
-        final_prob = softmax(logits)
-        final = final_prob.argmax(dim=-1)
-        final[np.amax(np.array(final_prob.cpu()), axis=-1) < UNASSIGN_THRES] = -1
-        predictions.append(final)
-        truths.append(labels_v)
-
-    predictions = distributed_concat(torch.cat(predictions, dim=0), len(val_sampler.dataset), world_size)
-    truths = distributed_concat(torch.cat(truths, dim=0), len(val_sampler.dataset), world_size)
-    no_drop = predictions != -1
-    predictions = np.array((predictions[no_drop]).cpu())
-    truths = np.array((truths[no_drop]).cpu())
-    cur_acc = accuracy_score(truths, predictions)
-    f1 = f1_score(truths, predictions, average='macro')
-    val_loss = running_loss / index
-    val_loss = get_reduced(val_loss, local_rank, 0, world_size)
-
-    if is_master:
-        print(f'== Validation | Loss: {val_loss:.6f} | F1 Score: {f1:.6f} ==')
-        print(confusion_matrix(truths, predictions))
-        print(classification_report(truths, predictions, target_names=label_dict.tolist(), digits=4))
