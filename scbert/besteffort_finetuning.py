27a504c5e756442cdbd6579e58f0d55241220878
Amrithya.B <amrithyarao1999@gmail.com>
2025-05-23 14:44:54 +0200

    Update besteffort_finetuning.py

--- scbert/besteffort_finetuning.py
+++ scbert/besteffort_finetuning.py
@@ -50,21 +50,17 @@ PATIENCE = 10
 model_name = args.model_name
 ckpt_dir = args.ckpt_dir
 
-#local_rank = args.local_rank
-local_rank = int(os.environ.get('LOCAL_RANK', os.environ.get('SLURM_LOCALID', args.local_rank)))
+local_rank = args.local_rank
 is_master = local_rank == 0
 
-dist.init_process_group(backend='nccl',init_method='env://')
+dist.init_process_group(backend='nccl')
 torch.cuda.set_device(local_rank)
 device = torch.device("cuda", local_rank)
 world_size = torch.distributed.get_world_size()
 
-print(f"[Rank {dist.get_rank()}] Local Rank: {local_rank}, Using Device: {torch.cuda.current_device()}")
-print(f"[Rank {dist.get_rank()}] CUDA_VISIBLE_DEVICES={os.environ.get('CUDA_VISIBLE_DEVICES')}")
-
-
 CLASS = args.bin_num + 2
 SEQ_LEN = args.gene_num + 1
+
 
 class SCDataset(Dataset):
     def __init__(self, data, label):
