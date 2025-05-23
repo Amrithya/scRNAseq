987207ac4264b045040812450f621cb492f588a1
Amrithya.B <amrithyarao1999@gmail.com>
2025-05-23 16:54:17 +0200

    Update testing.py

--- scbert/testing.py
+++ scbert/testing.py
@@ -1,16 +1,13 @@
+import os
 import torch
 import torch.distributed as dist
-import argparse
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--local_rank", type=int)
-    args = parser.parse_args()
-
-    torch.cuda.set_device(args.local_rank)
+    local_rank = int(os.environ["LOCAL_RANK"]) 
+    torch.cuda.set_device(local_rank)
     dist.init_process_group("nccl")
     rank = dist.get_rank()
-    print(f"Hello from rank {rank} on device {args.local_rank}")
+    print(f"Hello from rank {rank} on device {local_rank}")
 
 if __name__ == "__main__":
     main()
