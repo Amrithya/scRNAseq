107c34f094dacee517595a5caacc0e64e1f84f6f
Amrithya <amrithyarao1999@gmail.com>
2025-05-26 14:34:53 +0200

    besteffort

--- scbert/besteffort_finetuning.sh
+++ scbert/besteffort_finetuning.sh
@@ -11,6 +11,8 @@
 #SBATCH --error=results/besteffort_finetune_%A_%a.err
 #SBATCH --requeue
 
+echo $CUDA_VISIBLE_DEVICES
+
 NCCL_DEBUG=INFO CUDA_VISIBLE_DEVICES=0,1,2,3 CUDA_LAUNCH_BLOCKING=1 poetry run torchrun --nproc_per_node=4 --nnodes=1 --node_rank=0 besteffort_finetuning.py \
     --data_path "/data1/data/corpus/Zheng68K.h5ad" \
     --model_path "/data1/data/corpus/panglao_pretrain.pth"
