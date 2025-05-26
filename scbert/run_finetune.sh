9c86543cda0008183b2fe4ca19deb84baedb81ec
Amrithya.B <amrithyarao1999@gmail.com>
2025-05-26 23:32:04 +0200

    Update run_finetune.sh

--- scbert/run_finetune.sh
+++ scbert/run_finetune.sh
@@ -1,31 +1,21 @@
 #!/bin/bash
 
-#SBATCH --job-name="scBERT_finetune"          # Job Name
-
-#SBATCH --partition=gpu                     # Partition name
-
+#SBATCH --job-name="scBERT_finetune"
+#SBATCH --partition=gpu
 #SBATCH --gres=gpu:2
-
 #SBATCH --nodes=1
-
 #SBATCH --ntasks-per-node=1
-
 #SBATCH --cpus-per-task=3
-
-#SBATCH --time=24:00:00                     # Max runtime (HH:MM:SS)
-
+#SBATCH --time=24:00:00
 #SBATCH --hint=nomultithread
+#SBATCH --output=results/scBERT_finetune_%A_%a.out
+#SBATCH --error=results/scBERT_finetune_%A_%a.err
+#SBATCH --array=1
 
-#SBATCH --output=results/scBERT_finetune_%A_%a.out    # STDOUT file
 
-#SBATCH --error=results/scBERT_finetune_%A_%a.err     # STDERR file
+poetry run python -m torch.distributed.launch --nproc_per_node=2 finetune.py \
+    --data_path "/data1/data/corpus/Zheng68K.h5ad" \
+    --model_path "/data1/data/corpus/panglao_pretrain.pth"
 
-#SBATCH --array=1                       # Array job indices
-
-echo "gooo"
-poetry run python -u -m --nproc_per_node=2 torch.distributed.launch finetune.py \
-	--data_path "/data1/data/corpus/Zheng68K.h5ad" \
-	--model_path "/data1/data/corpus/panglao_pretrain.pth"
-
-echo "All Done!"
+echo "All Done at $(date)!"
 wait
