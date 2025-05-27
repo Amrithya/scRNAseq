948b31660ff930d61bb76f3146081f1d8fb75741
Amrithya <amrithyarao1999@gmail.com>
2025-05-27 15:27:17 +0200

    xai

--- cell_type_classification/test.py
+++ cell_type_classification/test.py
@@ -23,15 +23,24 @@ cell_type_series = adata.obs['cell_type']
 le = LabelEncoder()
 y = le.fit_transform(cell_type_series)
 
+print(f"X shape: {X.shape}")
+print(f"y shape: {y.shape}")
+
 print("Splitting train/test...")
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+print(f"X_train shape: {X_train.shape}")
+print(f"X_test shape: {X_test.shape}")
+print(f"y_train shape: {y_train.shape}")
+print(f"y_test shape: {y_test.shape}")
+
+
 print("Fitting logistic regression...")
-clf = LogisticRegression(penalty="l1", solver="saga", multi_class="multinomial", max_iter=1000)
+clf = LogisticRegression(penalty="l1", C=0.1, solver="liblinear")
 clf.fit(X_train, y_train)
 
 print("Computing SHAP values...")
-explainer = shap.LinearExplainer(clf, X_train, feature_perturbation="interventional")
+explainer = shap.LinearExplainer(clf, X_train)
 shap_values = explainer.shap_values(X_test)
 
 print("Processing SHAP output...")
