6827d1095c192a3cd32d21a54e9c24167fda2011
Amrithya <amrithyarao1999@gmail.com>
2025-05-27 10:04:35 +0200

    besteffort

--- scbert/besteffort_finetuning.sh
+++ scbert/besteffort_finetuning.sh
@@ -14,9 +14,16 @@
 
 echo $CUDA_VISIBLE_DEVICES
 
+RESUME_FLAG=""
+if [ -f ckpts/finetune_latest.pth ]; then
+    echo "Checkpoint found. Resuming training..."
+    RESUME_FLAG="--resume"
+fi
+
 CUDA_LAUNCH_BLOCKING=1 poetry run torchrun --nproc_per_node=4 besteffort_finetuning.py \
     --data_path "/data1/data/corpus/Zheng68K.h5ad" \
-    --model_path "/data1/data/corpus/panglao_pretrain.pth"
+    --model_path "/data1/data/corpus/panglao_pretrain.pth" \
+    --resume $RESUME_FLAG
 
 echo "All Done!"
 wait
