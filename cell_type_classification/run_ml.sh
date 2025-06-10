c07d88f68ca39d1f6be27864ea7d4e397a9ddd1c
Amrithya <amrithyarao1999@gmail.com>
2025-06-10 10:57:13 +0200

    lrp

--- cell_type_classification/run_ml.sh
+++ cell_type_classification/run_ml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=lr
+#SBATCH --job-name=nn
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
@@ -8,8 +8,8 @@
 #SBATCH --cpus-per-task=3
 #SBATCH --time=24:00:00
 #SBATCH --hint=nomultithread
-#SBATCH --output=results/script_results/lr_%A_%a.out
-#SBATCH --error=results/script_results/lr_%A_%a.err
+#SBATCH --output=results/script_results/nn_%A_%a.out
+#SBATCH --error=results/script_results/nn_%A_%a.err
 #SBATCH --array=1
 
 
